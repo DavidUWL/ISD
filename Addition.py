a0a2ac43e60f9b010b43311ceb6f4174bc8ea45b
DavidUWL <davidkirbymusician@gmail.com>
2015-10-01 13:31:13 +0100

    Create Addition.py

---
+++ Addition.py
@@ -0,0 +1,8 @@
+additionone = int(input("please enter your first number"))
+additiontwo = int(input("please enter your second number"))
+
+result= 0
+
+result = additionone+additiontwo
+
+print ("your output is", result)
